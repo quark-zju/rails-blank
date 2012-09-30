531ef8fdd16de2d0eb12a45a0996e86ccc1bacb8
WU Jun <quark@lihdd.net>
2012-09-30 18:11:27 +0800

    initial import: rails 3.2.8 default template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsNew::Application.config.secret_token = '021b03de120420e3d5a27b969b375fb6279be3403c6e255180983df9a9530182cc016e4904183e9c5c1cd3f9b4f6362e4e5241152063f6f4dbb2695ac4995372'
