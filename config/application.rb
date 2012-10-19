832a1da45d5e8f99eb32d1aff3475fec1b9db0ac
WU Jun <quark@lihdd.net>
2012-10-20 03:17:50 +0800

    use slim as default template engine

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module RailsNew
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # use slim as default template engine
+    config.generators.template_engine = :slim
   end
 end
